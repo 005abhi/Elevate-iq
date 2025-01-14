18a9a4e987be45af0cd3f268fa7f8a30b5605a02
005abhi <155890989+005abhi@users.noreply.github.com>
2025-01-14 19:55:44 +0530

    Update ai.ts

--- src/app/Gameit/Aiquizbot/lib/ai.ts
+++ src/app/Gameit/Aiquizbot/lib/ai.ts
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// const genAI = new GoogleGenerativeAI("AIzaSyA4ZQG1CcTcTDahJHKuEjtgHFMpyFpb5kg");
+const genAI = new GoogleGenerativeAI("AIzaSyA4ZQG1CcTcTDahJHKuEjtgHFMpyFpb5kg");
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 export const createPrompt = async (title: string) => {
